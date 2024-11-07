42cd4ea48bf8e5308d770b9087724dabed0c4c67
Vishal Kushwah <vishal.kushwah@xemailverify.com>
2024-11-08 01:53:23 +0530

    ec2 & variable file change to ohio region

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("/Users/vishalkushwah/Desktop/megaProject/Wanderlust-Mega-Project/terraform/vishal.pub")
 }
 
 resource "aws_default_vpc" "default" {
